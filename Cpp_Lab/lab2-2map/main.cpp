ecb65c2b9f142d33b664cecc0348574bed6ef1f4
Penut Chen <penut85420@gmail.com>
2016-04-14 11:27:47 +0800

    hi

---
+++ Cpp_Lab/lab2-2map/main.cpp
@@ -0,0 +1,39 @@
+﻿#include <cstdio>
+#include <cstring>
+#include <string>
+#include <map>
+#include <string>
+using namespace std;
+
+char s[1000001];
+
+int main() {
+    int N;
+    map<string, int> cont;
+    
+    while (scanf("%d%s",&N,s) == 2) {
+        int L = strlen(s);
+        cont.clear();
+        
+        for (int i=0; i+N<=L; ++i) {
+			char tmp[1000001];
+            strcpy(tmp, s);
+			tmp[N + i] = 0;
+            cont[tmp + i]++;
+        }
+        
+        string ans_hash;
+        int best = -1;
+        
+        for (map<string, int>::iterator it=cont.begin(); it!=cont.end(); ++it) {
+            if (it->second > best) {
+                ans_hash = it->first;
+                best = it->second;
+            }
+        }
+        
+        printf("%s:%d\n", ans_hash.c_str(), best);
+    }
+    
+    return 0;
+}
